9bfa4c1f0e03c72425f3c08f8003552636cfa51f
Your Name <your@email.com>
2022-03-26 18:58:20 +0300

    add

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 
 
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 443))
 TOKEN = '5178260352:AAHVZ1RI6YfNdUIwHczMq7CIMv2Z0dw8xTI'
 
 # Enable logging
@@ -183,10 +183,15 @@ def main() -> None:
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
+    # updater.start_webhook(listen="0.0.0.0",
+    #                       port=int(PORT),
+    #                       url_path=TOKEN)
+    # updater.bot.setWebhook('https://ancient-waters-23105.herokuapp.com/' + TOKEN)
+
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://ancient-waters-23105.herokuapp.com/' + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url='https://yourherokuappname.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
